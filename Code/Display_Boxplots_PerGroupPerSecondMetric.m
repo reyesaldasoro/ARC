5ec93c811f77ed9a9ce890419345438937557a60
CCRA <ccr22@cm3-1-01-94.managed.city.ac.uk>
2019-04-30 14:17:05 +0100

    Update Display_Boxplots_PerGroupPerSecondMetric.m
    Add the subset of groups

--- Code/Display_Boxplots_PerGroupPerSecondMetric.m
+++ Code/Display_Boxplots_PerGroupPerSecondMetric.m
@@ -21,7 +21,7 @@ load(CumulativeStats_Dir(end).name,'cumulativeStats','labels')
 %labels={'group','case','time','Dist [um/s]','Rel Position','Min/Maj','Forkness (N)','Forkness (C)','Skel Alignment'};
 
 %% Display ONE METRIC against the groups 1,2,3,... 14 AND A second metric divided by steps in boxplots
-step = 2;
+step = 5;
 invStep = 1/step;
 currentMetric_x           = 4;
 currentMetric_y           = 5;
@@ -29,4 +29,21 @@ boxplot(cumulativeStats(:,currentMetric_y) ,{cumulativeStats(:,1),invStep/2+(inv
 grid on
 xlabel({'Group';labels{currentMetric_x}},'fontsize',15)
 ylabel(labels{currentMetric_y},'fontsize',20)
+
+%% Display ONE METRIC against A SUBSET of the groups 1,2,3,... 14  AND A second metric divided by steps in boxplots
+step = 5;
+invStep = 1/step;
+
+currentMetric_x             = 4;
+currentMetric_y             = 6;
+subsetGroups                = [1 3 6 14 16];
+%figure
+indexGroups                 = ismember(cumulativeStats(:,1),subsetGroups);
+boxplot(cumulativeStats(indexGroups,currentMetric_y),{cumulativeStats(indexGroups,1),invStep/2+(invStep)*round(step*cumulativeStats(indexGroups,currentMetric_x))},'whisker',1)
+grid on
+xlabel(labels{currentMetric_x},'fontsize',20)
+ylabel(labels{currentMetric_y},'fontsize',20)
+ %axis([0.5 5.5 -0.75 0.6])
+ %axis([0.25 10.75 -0.75 0.65])
+
 
